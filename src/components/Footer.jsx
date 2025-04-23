1a9786ca4c0dd0f26e74bf493b296b22a083cd05
PaulImmanuel <paulimmanuel@karunya.edu.in>
2025-04-23 15:23:53 +0530

    flower

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
     return(
         <div>
         <footer className="text-center py-6 bg-gray-800 text-white">
-            <p>&copy; 2025  Electricity Board Management inventory. All rights reserved.</p>
+            <p>&copy; 2025  Flower Management inventory. All rights reserved.</p>
         </footer>
         </div>
     )
